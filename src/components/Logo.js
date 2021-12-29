5c9d95d5a3239ac8397f132128fbe7fca6ef7a08
Juan <juanantonioamillopez@gmail.com>
2021-12-29 11:04:07 +0100

    Añadimos logo a página

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,14 +2,28 @@ import { Component } from "react";
 
 const estilo = {
     logo: {
+        maxHeight: "100px",
+        maxWidth: "100px",
         fontWeight: "700",
         fontSize: "2rem",
+    },
+
+    img: {
+        width: "100%",
     },
 };
 
 class Logo extends Component {
     render() {
-        return <div style={estilo.logo}>&#127813;</div>;
+        return (
+            <div style={estilo.logo}>
+                <img
+                    style={estilo.img}
+                    src="/productos/logoFruta.png"
+                    alt="[logo]"
+                />
+            </div>
+        );
     }
 }
 
